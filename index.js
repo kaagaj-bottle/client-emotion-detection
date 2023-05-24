55c6f250af8e5ca148a5d10249d4d1adcd47c45d
Zishan Siddique <zishansiddique80@gmail.com>
2023-05-24 13:14:21 +0545

    init

---
+++ index.js
@@ -0,0 +1,57 @@
+let form = document.querySelector("#upload");
+let file = document.querySelector("#file");
+let app = document.querySelector("#app");
+let result=document.querySelector("#result")
+const url = "http://127.0.0.1:5000";
+
+function convertImageToBase64(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = reject;
+    reader.readAsDataURL(file);
+  });
+}
+
+function logFile(event) {
+  let str = event.target.result;
+  let img = document.createElement("img");
+  img.src = str;
+  img.height=100
+  app.append(img);
+}
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  if (!file.value.length) return;
+
+  // Create a new FileReader() object
+  let reader = new FileReader();
+
+  // Setup the callback event to run when the file is read
+  reader.onload = logFile;
+
+  // Read the file
+  reader.readAsDataURL(file.files[0]);
+
+  imageInput = file.files[0];
+  convertImageToBase64(imageInput).then((base64Image) => {
+    console.log(base64Image)
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ image: base64Image }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  });
+};
+
+form.addEventListener("submit", handleSubmit);
